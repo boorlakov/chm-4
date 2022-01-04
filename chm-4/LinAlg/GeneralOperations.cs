65f4d9440a161ee01b4cf0bc736a183cbe6b88f5
boorlakov <vladimir.boorlakov@gmail.com>
2022-01-04 17:39:26 +0700

    ♻️ Add comment

--- chm-4/LinAlg/GeneralOperations.cs
+++ chm-4/LinAlg/GeneralOperations.cs
@@ -2,5 +2,10 @@ namespace chm_4.LinAlg;
 
 public static class GeneralOperations
 {
+    /// <summary>
+    /// Calculates Euclidean norm of vector in R 
+    /// </summary>
+    /// <param name="x">Vector with double elements</param>
+    /// <returns>Euclidean norm value of vector in R</returns>
     public static double Norm(double[] x) => Math.Sqrt(x.Sum(t => t * t));
 }
