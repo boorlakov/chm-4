fa872c691e21b61d5049af4c3053d60c8f7a7c4f
boorlakov <vladimir.boorlakov@gmail.com>
2022-01-04 17:28:52 +0700

    ⚰️ Delete dumb code

--- chm-4/Utils.cs
+++ chm-4/Utils.cs
@@ -4,32 +4,6 @@ namespace chm_4;
 
 public static class Utils
 {
-    private static double[] ReadDoubles(StreamReader file)
-    {
-        return file
-            .ReadLine()!
-            .Trim()
-            .Split(' ')
-            .Select(double.Parse)
-            .ToArray();
-    }
-
-    private static int[] ReadInts(StreamReader file)
-    {
-        return file
-            .ReadLine()!
-            .Trim()
-            .Split(' ')
-            .Select(int.Parse)
-            .ToArray();
-    }
-
-    /// <summary>
-    /// Read vector from file, values separated by comma. Use another name for more concrete sense
-    /// </summary>
-    /// <param name="file">Already opened file</param>
-    /// <returns>Static double array</returns>
-    public static double[] VectorFromFile(StreamReader file) => ReadDoubles(file);
 
     /// <summary>
     /// Exports double static array to already opened file
